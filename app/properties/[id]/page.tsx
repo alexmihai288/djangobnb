3e7844b22d70b428ce4bddf649bf7fc8207115d4
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-06 22:08:27 +0300

    chore - reservation sidebar

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const PropertyDetaiPage = () => {
   return (
     <main className="max-w-[1500px] mx-auto px-6">
-      <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
+      <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
         <Image
           src="/beach_1.jpg"
           fill
@@ -14,7 +14,7 @@ const PropertyDetaiPage = () => {
         />
       </div>
 
-      <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         <div className="py-6 pr-6 col-span-3">
           <h1 className="mb-4 text-4xl">Property name</h1>
           <span className="mb-6 block text-lg text-gray-600">
