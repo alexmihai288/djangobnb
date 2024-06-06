e344584df2154e19ff9d98292c3c975fb0bcacda
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-06 21:01:35 +0300

    chore -m property list

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -4,13 +4,41 @@ import React from "react";
 export const Categories = () => {
   return (
     <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
         <Image
           src="/icn_category_beach.jpeg"
           alt="Category - Beach"
           width={20}
           height={20}
         />{" "}
+        Beach
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />{" "}
+        Villas
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />{" "}
+        Cabins
+      </div>
+      <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />{" "}
+        Tiny homes
       </div>
     </div>
   );
---
+++ app/components/properties/PropertyListItem.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+export const PropertyListItem = () => {
+  return (
+    <div className="cursor-pointer">
+      <div className="relative overflow-hidden aspect-square rounded-xl">
+        <Image
+          fill
+          src="/beach_1.jpg"
+          sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
+          className="hover:scale-110 object-cover transition h-full w-full"
+          alt="Beach house"
+        />{" "}
+      </div>
+    </div>
+  );
+};
