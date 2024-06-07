2d1cdd8cd55bfe3111823ece165fec52e005520b
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-07 22:28:38 +0300

    chore landlord

--- app/landlords/[id]/page.tsx
+++ app/landlords/[id]/page.tsx
@@ -19,10 +19,10 @@ const LandLordPage = () => {
             <ContactButton />
           </div>
         </aside>
-        <div className="col-span-3 pl-0 md:pl-6">
-        <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-        <PropertyList />
-      </div>
+        <div className="col-span-1 md:col-span-3 pl-0 md:pl-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <PropertyList /> 
+          </div>
         </div>
       </div>
     </main>
