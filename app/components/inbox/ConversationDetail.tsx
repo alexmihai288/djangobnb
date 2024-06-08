70e8aebb0e2eaa3dd4a2ba01d01615314f458594
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-06-08 22:06:50 +0300

    chore conve

---
+++ app/components/inbox/ConversationDetail.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ConversationDetail = () => {
+  return (
+    <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+      <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+        <p className="font-bold text-gray-500">John Doe</p>
+
+        <p>djaisdjasjdia</p>
+      </div>
+    </div>
+  );
+};
